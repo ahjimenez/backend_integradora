4256d79e094737f453c951bf860679511b1eddce
unknown <marco07121999@gmail.com>
2025-07-10 18:09:08 -0600

    modulo-attachments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AttachmentsModule } from './modules/attachments/attachments.module';
 
 @Module({
-  imports: [],
+  imports: [AttachmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/attachments/infraestructure/repositories/prisma-attachment.repository.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from "@nestjs/common";
+import { IAttachmentRepository } from "../../domain/interfaces/attachment-repository.interface";
+import { PrismaService } from "src/core/databases/prisma.service";
+import { Attachment } from "../../domain/entities/attachment";
+import { AttachmentMapper } from "../mappers/attachment.mapper.ts";
+
+@Injectable()
+export class PrismaAttachmentsRepository implements IAttachmentRepository {
+
+    constructor(private readonly prismaService: PrismaService) { }
+
+    async create(attachment: Attachment): Promise<Attachment | null> {
+
+      const newAttachment = await this.prismaService.attachment.create({
+            data: {
+              ...attachment,
+              id:undefined,
+            }
+        }); 
+   
+        return AttachmentMapper.toDomain(newAttachment); 
+    }
+
+}
